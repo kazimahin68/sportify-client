8f8ba0c066269ef2031b2efcfd92370c2c8a6979
kazimahin68 <kazimahin68@gmail.com>
2023-06-09 23:18:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='mt-32'>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Button</button>
+        <button className="btn btn-secondary">Button</button>
+        <button className="btn btn-accent">Button</button>
+        <button className="btn btn-ghost">Button</button>
+        <button className="btn btn-link">Button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
