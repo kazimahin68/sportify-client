77030e7c642bcbddfde1185e654097271d0f2bca
kazimahin68 <kazimahin68@gmail.com>
2023-06-11 02:44:09 +0600

    firebase connect and authProvider added

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,61 @@
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react"
+import app from "../firebase/firebase.config";
+
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null)
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const loggedIn = (email, password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logOut = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            setLoading(false)
+        });
+        return () =>{
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        createUser,
+        loggedIn,
+        logOut,
+        googleLogin,
+        loading,
+
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
